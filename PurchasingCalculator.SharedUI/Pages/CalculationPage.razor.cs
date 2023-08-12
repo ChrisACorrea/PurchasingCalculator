acc4353f38faf1484784532ff15469e2fef23c76
Christopher Corrêa <christopher.a.c@outlook.com>
2023-08-12 14:10:56 -0300

    feat: Adicionado nova tela de cálculo

---
+++ PurchasingCalculator.SharedUI/Pages/CalculationPage.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using PurchasingCalculator.Shared.Models;
+using System.Globalization;
+
+namespace PurchasingCalculator.SharedUI.Pages;
+
+public partial class CalculationPage : ComponentBase
+{
+    public Purchase Purchase { get; set; } = new();
+    public PurchaseItem EditingItem = new();
+
+    public void AddItem()
+    {
+        Purchase.AddItem(EditingItem);
+        ClearEditingItem();
+    }
+
+    public void ClearEditingItem()
+        => EditingItem = new();
+
+    public bool HasDecimalPoint(decimal number)
+        => number.ToString().Contains(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+}
---
+++ PurchasingCalculator.SharedUI/Pages/Index.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PurchasingCalculator.SharedUI.Pages;
+
+public partial class Index : ComponentBase
+{
+    [Inject]
+    public NavigationManager Navigation { get; set; } = null!;
+
+    public void GoToCalculationPage() =>
+        Navigation.NavigateTo("calculation");
+}
