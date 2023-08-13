bcf9e2645662f9df4f937df72c44a61d3d6a5fce
Christopher Corrêa <christopher.a.c@outlook.com>
2023-08-13 15:00:18 -0300

    feat: Melhorado usabilidade do formulário na tela de cálculo.

--- PurchasingCalculator.SharedUI/Pages/CalculationPage.razor.cs
+++ PurchasingCalculator.SharedUI/Pages/CalculationPage.razor.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
 using PurchasingCalculator.Shared.Models;
 using System.Globalization;
 
@@ -9,15 +11,24 @@ public partial class CalculationPage : ComponentBase
     public Purchase Purchase { get; set; } = new();
     public PurchaseItem EditingItem = new();
 
-    public void AddItem()
+    private MudTextField<string> descriptionField = null!;
+    private MudNumericField<decimal> priceField = null!;
+    private MudNumericField<decimal> quantityField = null!;
+
+    private void OnSubmit(EditContext context)
     {
+        if (EditingItem.TotalPrice <= 0) return;
+
         Purchase.AddItem(EditingItem);
         ClearEditingItem();
     }
 
     public void ClearEditingItem()
-        => EditingItem = new();
+    {
+        EditingItem = new();
+        StateHasChanged();
+    }
 
-    public bool HasDecimalPoint(decimal number)
+    public static bool HasDecimalPoint(decimal number)
         => number.ToString().Contains(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
 }
